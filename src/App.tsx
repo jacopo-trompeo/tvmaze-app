0321cbec6882290cbdd70dd47f1c0452690542ab
Jacopo Trompeo <jacopo.trompeo@gmail.com>
2022-11-19 00:48:35 +0100

    Added signup page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
 import SearchPage from "./pages/SearchPage";
 import { AuthContextProvider } from "./context/AuthContext";
 import ProtectedRoute from "./context/ProtectedRoute";
@@ -10,6 +11,14 @@ const router = createBrowserRouter([
 		element: (
 			<ProtectedRoute isAuth={true}>
 				<LoginPage />
+			</ProtectedRoute>
+		),
+	},
+	{
+		path: "/signup",
+		element: (
+			<ProtectedRoute isAuth={true}>
+				<SignupPage />
 			</ProtectedRoute>
 		),
 	},
--- src/pages/SignupPage.tsx
+++ src/pages/SignupPage.tsx
@@ -5,14 +5,15 @@ import { Link, useNavigate } from "react-router-dom";
 const LoginPage = () => {
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
+	const [confirmPassword, setConfirmPassword] = useState("");
 	const navigate = useNavigate();
-	const { logIn } = useAuth();
+	const { createUser } = useAuth();
 
 	const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
 
 		try {
-			await logIn(email, password);
+			await createUser(email, password);
 			navigate("/");
 		} catch (err: any) {
 			console.log(err);
@@ -22,19 +23,19 @@ const LoginPage = () => {
 	return (
 		<main className="flex flex-col justify-center items-center h-screen bg-gray-100 px-10">
 			<h2 className="text-center text-4xl font-extrabold text-gray-900">
-				Sign in to TVMaze App
+				Sign up to TVMaze App
 			</h2>
-			<p className="mt-4 text-center text-sm text-gray-600">
-				Don't have an account?{" "}
+			<p className="mt-4 mb-8 text-center text-sm text-gray-600">
+				Already have an account?{" "}
 				<Link
-					to="/signup"
+					to="/login"
 					className="font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:border-b focus:border-indigo-500"
 				>
-					Sign up here.
+					Sign in here.
 				</Link>
 			</p>
 
-			<div className="mt-8 w-full md:max-w-md mx-auto bg-white shadow-md rounded-lg py-10 px-6">
+			<div className="w-full md:max-w-md mx-auto bg-white shadow-md rounded-lg py-10 px-6">
 				<form onSubmit={handleSubmit}>
 					<div className="flex flex-col py-2">
 						<label
@@ -44,10 +45,10 @@ const LoginPage = () => {
 							Email
 						</label>
 						<input
-							type="email"
-							id="email"
 							value={email}
 							onChange={e => setEmail(e.target.value)}
+							type="email"
+							id="email"
 							className="border border-gray-300 px-3 py-2 rounded-lg focus:outline-none focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500"
 						/>
 					</div>
@@ -59,10 +60,26 @@ const LoginPage = () => {
 							Password
 						</label>
 						<input
-							type="password"
-							id="password"
 							value={password}
 							onChange={e => setPassword(e.target.value)}
+							type="password"
+							id="password"
+							className="border border-gray-300 px-3 py-2 rounded-lg focus:outline-none focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500"
+						/>
+					</div>
+
+					<div className="flex flex-col py-2">
+						<label
+							htmlFor="confirmPassword"
+							className="mb-1 text-sm font-medium text-gray-700 "
+						>
+							Confirm password
+						</label>
+						<input
+							value={confirmPassword}
+							onChange={e => setConfirmPassword(e.target.value)}
+							type="password"
+							id="confirmPassword"
 							className="border border-gray-300 px-3 py-2 rounded-lg focus:outline-none focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500"
 						/>
 					</div>
