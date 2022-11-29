3321a1e3e8ef11df94375de98e8925c2cd1ab145
Jacopo Trompeo <jacopo.trompeo@gmail.com>
2022-11-29 15:13:14 +0100

    Added search bar

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,25 +1,41 @@
-import { useNavigate } from "react-router-dom";
-import { useAuth } from "../context/AuthContext";
-
 const SearchPage = () => {
-	const { user, logOut } = useAuth();
-	const navigate = useNavigate();
-
-	const handleSignOut = async () => {
-		try {
-			await logOut();
-			navigate("/login");
-		} catch (error) {
-			console.log(error);
-		}
+	const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		console.log("searching...");
 	};
 
 	return (
-		<div>
-			{user && user.email}
-			<br />
-			<button onClick={handleSignOut}>Sign Out</button>
-		</div>
+		<main className="container mx-auto">
+			<h1 className="text-center font-bold text-4xl mt-5">
+				Search for a show
+			</h1>
+
+			<form className="mt-10" onSubmit={handleSearch}>
+				<div className="relative">
+					<input
+						type="text"
+						placeholder="Search by name..."
+						className="input input-bordered input-accent w-full"
+					/>
+					<button className="absolute right-5 top-3">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							strokeWidth="1.5"
+							stroke="currentColor"
+							className="w-6 h-6"
+						>
+							<path
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+							/>
+						</svg>
+					</button>
+				</div>
+			</form>
+		</main>
 	);
 };
 
