9e10b73f419bd5d0663bf57bbedceba22de6ea65
Jacopo Trompeo <jacopo.trompeo@gmail.com>
2022-11-29 20:08:01 +0100

    Added null check on show details

--- src/pages/ShowDetailPage.tsx
+++ src/pages/ShowDetailPage.tsx
@@ -5,9 +5,7 @@ import Navbar from "../components/Navbar";
 
 const ShowDetailPage = () => {
 	const { id } = useParams<{ id: string }>();
-	const [showDetails, setShowDetails] = useState<ShowDetailType>(
-		{} as ShowDetailType
-	);
+	const [showDetails, setShowDetails] = useState<ShowDetailType | null>(null);
 
 	useEffect(() => {
 		const showId = id ? parseInt(id) : null;
@@ -23,37 +21,59 @@ const ShowDetailPage = () => {
 	return (
 		<>
 			<Navbar />
-			<main className="container mx-auto grid grid-cols-1 md:grid-cols-2 gap-10">
-				<div>
-					<img
-						src={showDetails.image}
-						alt={showDetails.title}
-						className="w-full rounded-lg"
-					/>
-				</div>
-				<div>
-					<h1 className="text-4xl font-semibold">
-						{showDetails.title}
-					</h1>
-					<p className="mt-2 text-sm">
-						{showDetails.startDate || "N/A"} -{" "}
-						{showDetails.endDate || "N/A"}
-					</p>
-
-					<p className="mt-10">
-						{showDetails.summary?.replace(/(<([^>]+)>)/gi, "") ||
-							"No description available"}
-					</p>
-
-					<div className="mt-10 flex gap-3">
-						{showDetails.genres?.map((genre, i) => (
-							<span className="badge" key={i}>
-								{genre}
-							</span>
-						))}
+			{showDetails ? (
+				<main className="max-w-md px-5 md:px-0 md:container mx-auto grid grid-cols-1 md:grid-cols-2 gap-10">
+					<div>
+						<img
+							src={showDetails.image}
+							alt={showDetails.title}
+							className="w-full rounded-lg"
+						/>
 					</div>
-				</div>
-			</main>
+					<div className="flex flex-col justify-center">
+						<button className="btn btn-circle mb-2">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 24 24"
+								strokeWidth={1.5}
+								stroke="currentColor"
+								className="w-6 h-6"
+							>
+								<path
+									strokeLinecap="round"
+									strokeLinejoin="round"
+									d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18"
+								/>
+							</svg>
+						</button>
+						<h1 className="text-4xl font-semibold">
+							{showDetails.title}
+						</h1>
+						<p className="mt-2 text-sm">
+							{showDetails.startDate || "N/A"} -{" "}
+							{showDetails.endDate || "N/A"}
+						</p>
+
+						<p className="mt-10 text-justify">
+							{showDetails.summary?.replace(
+								/(<([^>]+)>)/gi,
+								""
+							) || "No description available"}
+						</p>
+
+						<div className="mt-10 flex gap-3">
+							{showDetails.genres?.map((genre, i) => (
+								<span className="badge" key={i}>
+									{genre}
+								</span>
+							))}
+						</div>
+					</div>
+				</main>
+			) : (
+				""
+			)}
 		</>
 	);
 };
