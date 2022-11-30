8fb480e221849455d0d0ad8c316025413795e7e7
Jacopo Trompeo <jacopo.trompeo@gmail.com>
2022-11-30 16:44:27 +0100

    Displaying favorite shows in profile page

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,9 +1,18 @@
 import Navbar from "../components/Navbar";
+import ShowsList from "../components/ShowsList";
+import useFavorites from "../hooks/useFavorites";
+import useFavoriteShows from "../hooks/useFavoriteShows";
 
 const ProfilePage = () => {
+	const favoritesIds = useFavorites();
+	const favoriteShows = useFavoriteShows({ favoritesIds });
+
 	return (
 		<>
 			<Navbar />
+			<main className="max-w-md px-5 sm:px-0 sm:container mx-auto">
+				<ShowsList shows={favoriteShows} />
+			</main>
 		</>
 	);
 };
