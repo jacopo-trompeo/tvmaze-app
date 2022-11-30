437b2aea975ae96c91c1e861f5b48ed55ae0b724
Jacopo Trompeo <jacopo.trompeo@gmail.com>
2022-11-30 20:58:55 +0100

    Shows are now displayed in a card component

---
+++ src/components/ShowCard.tsx
@@ -0,0 +1,61 @@
+import { Link } from "react-router-dom";
+import { ShowType } from "../api";
+import { ref, remove, update } from "firebase/database";
+import { useAuth } from "../context/AuthContext";
+import { database } from "../firebase";
+import HeartIcon from "./icons/HeartIcon";
+import HeartOutlineIcon from "./icons/HeartOutlineIcon";
+
+interface PropTypes {
+	show: ShowType;
+	isFavorite: boolean;
+}
+
+const ShowCard = ({ show, isFavorite }: PropTypes) => {
+	const { user } = useAuth();
+
+	const addToFavorites = (showId: number) => {
+		update(ref(database, `users/${user?.uid}/favorites`), {
+			[showId]: showId,
+		});
+	};
+
+	const removeFromFavorites = (showId: number) => {
+		remove(ref(database, `users/${user?.uid}/favorites/${showId}`));
+	};
+
+	return (
+		<div className="card bg-base-300 shadow-xl image-full">
+			<figure className="max-h-[15rem]">
+				<img
+					src={show.image}
+					alt={show.title}
+					className="h-full w-full object-cover "
+				/>
+			</figure>
+			<div className="card-body">
+				<h2 className="card-title text-2xl">{show.title}</h2>
+
+				<div className="card-actions justify-end items-center gap-5 mt-auto">
+					<Link to={`/show/${show.id}`} className="btn btn-outline">
+						Go to details{" "}
+					</Link>
+					{isFavorite ? (
+						<button
+							className="text-accent"
+							onClick={() => removeFromFavorites(show.id)}
+						>
+							<HeartIcon />
+						</button>
+					) : (
+						<button onClick={() => addToFavorites(show.id)}>
+							<HeartOutlineIcon />
+						</button>
+					)}
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default ShowCard;
--- src/components/ShowsList.tsx
+++ src/components/ShowsList.tsx
@@ -1,66 +1,22 @@
-import { ref, remove, update } from "firebase/database";
-import { Link } from "react-router-dom";
 import { ShowType } from "../api";
-import { useAuth } from "../context/AuthContext";
-import { database } from "../firebase";
 import useFavorites from "../hooks/useFavorites";
-import HeartIcon from "./icons/HeartIcon";
-import HeartOutlineIcon from "./icons/HeartOutlineIcon";
+import ShowCard from "./ShowCard";
 
 interface PropTypes {
 	shows: ShowType[];
 }
 
 const ShowsList = ({ shows }: PropTypes) => {
-	const { user } = useAuth();
 	const favoritesIds = useFavorites();
-
-	const addToFavorites = (showId: number) => {
-		update(ref(database, `users/${user?.uid}/favorites`), {
-			[showId]: showId,
-		});
-	};
-
-	const removeFromFavorites = (showId: number) => {
-		remove(ref(database, `users/${user?.uid}/favorites/${showId}`));
-	};
 
 	return (
 		<div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-5 my-8">
 			{shows.map((show, i) => (
-				<div className="card bg-base-300 shadow-xl image-full" key={i}>
-					<figure className="max-h-[15rem]">
-						<img
-							src={show.image}
-							alt={show.title}
-							className="h-full w-full object-cover "
-						/>
-					</figure>
-					<div className="card-body">
-						<h2 className="card-title text-2xl">{show.title}</h2>
-
-						<div className="card-actions justify-end items-center gap-5 mt-auto">
-							<Link
-								to={`/show/${show.id}`}
-								className="btn btn-outline"
-							>
-								Go to details{" "}
-							</Link>
-							{favoritesIds.includes(show.id) ? (
-								<button
-									className="text-accent"
-									onClick={() => removeFromFavorites(show.id)}
-								>
-									<HeartIcon />
-								</button>
-							) : (
-								<button onClick={() => addToFavorites(show.id)}>
-									<HeartOutlineIcon />
-								</button>
-							)}
-						</div>
-					</div>
-				</div>
+				<ShowCard
+					key={i}
+					show={show}
+					isFavorite={favoritesIds.includes(show.id)}
+				/>
 			))}
 		</div>
 	);
