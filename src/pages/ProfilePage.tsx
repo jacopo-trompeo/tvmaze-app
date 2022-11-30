fc250aae1c6b3cfc03ac30baa17163fd01c784d2
Jacopo Trompeo <jacopo.trompeo@gmail.com>
2022-11-30 12:07:15 +0100

    Display favorites in user page

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,5 +1,50 @@
+import { useAuth } from "../context/AuthContext";
+import { database } from "../firebase";
+import { ref, onValue } from "firebase/database";
+import { useState, useEffect } from "react";
+import { ShowType, getShowById } from "../api";
+import Navbar from "../components/Navbar";
+import ShowsList from "../components/ShowsList";
+
 const ProfilePage = () => {
-	return <div>ProfilePage</div>;
+	const { user } = useAuth();
+	const [favorites, setFavorites] = useState<number[]>([]);
+	const [favoriteShows, setFavoriteShows] = useState<ShowType[]>([]);
+
+	useEffect(() => {
+		const favoritesRef = ref(database, `users/${user?.uid}/favorites`);
+		onValue(favoritesRef, (snapshot) => {
+			const data = snapshot.val();
+			if (!data) {
+				return;
+			}
+
+			setFavorites(Object.values(data));
+			getFavoriteShows(Object.values(data));
+		});
+	}, [user]);
+
+	const getFavoriteShows = (favorites: number[]) => {
+		const promises = favorites.map((id) => getShowById(id));
+		Promise.all(promises).then((shows) => {
+			setFavoriteShows(shows);
+		});
+
+		setFavoriteShows(favoriteShows);
+	};
+
+	return (
+		<main className="max-w-md px-5 sm:px-0 sm:container mx-auto">
+			<Navbar />
+
+			<ShowsList
+				shows={favoriteShows}
+				favorites={favorites}
+				addToFavorites={() => {}}
+				removeFromFavorites={() => {}}
+			/>
+		</main>
+	);
 };
 
 export default ProfilePage;
