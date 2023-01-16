a5964ccf9ebd11fb526d8949348933e5d23f0f25
Jacopo Trompeo <jacopo.trompeo@gmail.com>
2023-01-16 20:20:39 +0100

    Added back strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import React from "react";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-	<App />
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
 );
