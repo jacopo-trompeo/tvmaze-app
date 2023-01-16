4846e6a541ffb4fd637a66494cd870dc7a59c3be
Jacopo Trompeo <jacopo.trompeo@gmail.com>
2023-01-16 19:59:43 +0100

    Changed text color

--- src/components/ErrorAlert.tsx
+++ src/components/ErrorAlert.tsx
@@ -7,7 +7,7 @@ interface PropTypes {
 
 const ErrorAlert = ({ error, closeError }: PropTypes) => {
 	return (
-		<div className="flex mb-5 w-full py-2 px-4 rounded-md text-sm font-medium text-primary-content bg-error">
+		<div className="flex mb-5 w-full py-2 px-4 rounded-md text-sm font-medium bg-error">
 			<p>{error}</p>
 			<button
 				className="ml-auto cursor-pointer rounded-md focus:outline-none focus:ring-2 focus:ring-primary-content"
