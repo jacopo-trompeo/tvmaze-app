f57303cb90f3025f2efc8f65625dc251ffe98391
Jacopo Trompeo <jacopo.trompeo@gmail.com>
2023-01-17 23:52:25 +0100

    Added ranking to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,46 +3,49 @@ import { useAuth } from "../context/AuthContext";
 import ThemeSwitcher from "./ThemeSwitcher";
 
 const Navbar = () => {
-  const { logOut, user } = useAuth();
+	const { logOut, user } = useAuth();
 
-  return (
-    <nav className="navbar bg-base-100 mb-10 p-3 md:p-5 sticky top-0 z-50">
-      <div className="flex-1">
-        <Link
-          to={"/"}
-          className="normal-case text-xl sm:text-2xl md:text-4xl font-bold"
-        >
-          <span className="text-accent">TVMaze</span> App
-        </Link>
-      </div>
+	return (
+		<nav className="navbar bg-base-100 mb-10 p-3 md:p-5 sticky top-0 z-50">
+			<div className="flex-1">
+				<Link
+					to={"/"}
+					className="normal-case text-xl sm:text-2xl md:text-4xl font-bold"
+				>
+					<span className="text-accent">TVMaze</span> App
+				</Link>
+			</div>
 
-      <ThemeSwitcher className="mx-10" />
+			<ThemeSwitcher className="mx-10" />
 
-      <div className="dropdown dropdown-end">
-        <label
-          tabIndex={0}
-          className="btn btn-ghost md:btn-lg btn-circle avatar"
-        >
-          <div className="w-20 rounded-full">
-            <img
-              src={`https://avatars.dicebear.com/api/initials/${user?.email}.svg`}
-            />
-          </div>
-        </label>
-        <ul
-          tabIndex={0}
-          className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52 ring-1 ring-accent"
-        >
-          <li>
-            <Link to={"/profile"}>Profile</Link>
-          </li>
-          <li>
-            <button onClick={() => logOut()}>Logout</button>
-          </li>
-        </ul>
-      </div>
-    </nav>
-  );
+			<div className="dropdown dropdown-end">
+				<label
+					tabIndex={0}
+					className="btn btn-ghost md:btn-lg btn-circle avatar"
+				>
+					<div className="w-20 rounded-full">
+						<img
+							src={`https://avatars.dicebear.com/api/initials/${user?.email}.svg`}
+						/>
+					</div>
+				</label>
+				<ul
+					tabIndex={0}
+					className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52 ring-1 ring-accent"
+				>
+					<li>
+						<Link to={"/ranking"}>Ranking</Link>
+					</li>
+					<li>
+						<Link to={"/profile"}>Profile</Link>
+					</li>
+					<li>
+						<button onClick={() => logOut()}>Logout</button>
+					</li>
+				</ul>
+			</div>
+		</nav>
+	);
 };
 
 export default Navbar;
